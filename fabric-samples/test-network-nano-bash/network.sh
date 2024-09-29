68e3d73f515ed446ff0504759204a77daa6483fb
christian-amarildo <christianamarildo@hotmail.com>
2024-09-29 04:25:03 -0300

    comentarios adicionais

--- fabric-samples/test-network-nano-bash/network.sh
+++ fabric-samples/test-network-nano-bash/network.sh
@@ -4,10 +4,9 @@
 #
 set -eu
 
-# Função para exibir mensagens de ajuda sobre o uso do script
+# Print the usage message
 printHelp() {
   USAGE="${1:-}"
-  # Exibe a ajuda específica para o modo 'start'
   if [ "$USAGE" = "start" ]; then
     echo "Usage: "
     echo "  network.sh start [Flags]"
@@ -17,7 +16,6 @@ printHelp() {
     echo "    Flags:"
     echo "    -d <delay> - CLI delays for a certain number of seconds (defaults to 3)"
     echo "    -h - Print this message"
-  # Exibe a ajuda específica para o modo 'clean'
   elif [ "$USAGE" = "clean" ]; then
     echo "Usage: "
     echo "  network.sh clean [Flags]"
@@ -27,7 +25,6 @@ printHelp() {
     echo "    Flags:"
     echo "    -h - Print this message"
   else
-    # Exibe a ajuda padrão
     echo "Usage: "
     echo "  network.sh <Mode> [Flags]"
     echo "    Modes:"
@@ -42,114 +39,100 @@ printHelp() {
   fi
 }
 
-# Função para parar a rede Fabric
 networkStop() {
   echo "Stopping Fabric network..."
-  # Interrompe todos os processos em segundo plano
   trap " " 0 1 2 3 15 && kill -- -$$
   wait
   echo "Fabric network stopped."
 }
 
-# Função para iniciar a rede Fabric
 networkStart() {
-  # Define o atraso padrão para 5 segundos
   : "${CLI_DELAY:=5}"
 
-  # Configura um trap para parar a rede ao final do script
+  # shellcheck disable=SC2064
   trap networkStop 0 1 2 3 15
 
-  # Verifica se os artefatos já existem, caso contrário, gera-os
   if [ -d "${PWD}"/channel-artifacts ] && [ -d "${PWD}"/crypto-config ]; then
     echo "Using existing artifacts..."
     CREATE_CHANNEL=false
   else
     echo "Generating artifacts..."
-    ./generate_artifacts.sh  # Gera artefatos necessários para a rede
+    ./generate_artifacts.sh
     CREATE_CHANNEL=true
   fi
 
-  # Cria um diretório para logs
   echo "Creating logs directory..."
   mkdir -p "${PWD}"/logs
 
-  # Inicia os nós de ordenação em segundo plano, redirecionando logs
   echo "Starting orderers..."
   ./orderer1.sh > ./logs/orderer1.log 2>&1 &
   ./orderer2.sh > ./logs/orderer2.log 2>&1 &
   ./orderer3.sh > ./logs/orderer3.log 2>&1 &
 
-  # Aguarda um atraso antes de iniciar os peers
   echo "Waiting ${CLI_DELAY}s..."
   sleep ${CLI_DELAY}
 
-  # Inicia os nós de peer em segundo plano, redirecionando logs
   echo "Starting peers..."
   ./peer1.sh > ./logs/peer1.log 2>&1 &
   ./peer2.sh > ./logs/peer2.log 2>&1 &
   ./peer3.sh > ./logs/peer3.log 2>&1 &
   ./peer4.sh > ./logs/peer4.log 2>&1 &
 
-  # Aguarda outro atraso
   echo "Waiting ${CLI_DELAY}s..."
   sleep ${CLI_DELAY}
 
-  # Se os artefatos foram criados, cria o canal e junta os peers
   if [ "${CREATE_CHANNEL}" = "true" ]; then
     echo "Creating channel (peer1)..."
-    . ./peer1admin.sh && ./create_channel.sh  # Cria o canal
+    . ./peer1admin.sh && ./create_channel.sh
 
     echo "Joining channel (peer2)..."
-    . ./peer2admin.sh && ./join_channel.sh  # Junta o peer2 ao canal
+    . ./peer2admin.sh && ./join_channel.sh
 
     echo "Joining channel (peer3)..."
-    . ./peer3admin.sh && ./join_channel.sh  # Junta o peer3 ao canal
+    . ./peer3admin.sh && ./join_channel.sh
 
     echo "Joining channel (peer4)..."
-    . ./peer4admin.sh && ./join_channel.sh  # Junta o peer4 ao canal
+    . ./peer4admin.sh && ./join_channel.sh
   fi
 
   echo "Fabric network running. Use Ctrl-C to stop."
-  
-  # Aguarda os processos em segundo plano
+
   wait
 }
 
-# Função para limpar os diretórios da rede Fabric
 networkClean() {
   echo "Removing directories: channel-artifacts crypto-config data logs"
-  # Remove os diretórios especificados
   rm -r "${PWD}"/channel-artifacts || true
   rm -r "${PWD}"/crypto-config || true
   rm -r "${PWD}"/data || true
   rm -r "${PWD}"/logs || true
 }
 
-# Analisando argumentos da linha de comando
+# Parse commandline args
 
-# Verifica se há modo especificado
+## Parse mode
 if [ $# -lt 1 ] ; then
-  printHelp  # Exibe ajuda se não houver modo
+  printHelp
   exit 0
 else
-  MODE=$1  # Define o modo como o primeiro argumento
+  MODE=$1
   shift
 fi
 
-# Analisa as flags
+# parse flags
 while [ $# -ge 1 ] ; do
   key="$1"
   case $key in
   -d )
-    CLI_DELAY="$2"  # Define o atraso
+    CLI_DELAY="$2"
     shift
     ;;
   -h )
-    printHelp "$MODE"  # Exibe ajuda para o modo atual
+    printHelp "$MODE"
     exit 0
     ;;
   * )
-    echo "Unknown flag: $key"  # Trata flags desconhecidas
+    echo "Unknown flag: $key"
     printHelp
     exit 1
     ;;
@@ -157,12 +140,11 @@ while [ $# -ge 1 ] ; do
   shift
 done
 
-# Inicia a rede ou a limpa, dependendo do modo
 if [ "$MODE" = "start" ]; then
   networkStart
 elif [ "$MODE" = "clean" ]; then
   networkClean
 else
-  printHelp  # Exibe ajuda se o modo não for reconhecido
+  printHelp
   exit 1
 fi
